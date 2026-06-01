c297d4cb817f575636240cd7e89575ef09b7954f
Arjun Singh Negi <as9604793@gmail.com>
2026-06-01 21:25:25 +0530

    Refactor getUserProfile and and getUserRepos

--- Backend/Source/Services/Github.Service.js
+++ Backend/Source/Services/Github.Service.js
@@ -3,8 +3,8 @@ const cacheService = require('./Cache.Service')
 
 const GITHUB_API_BASE = "https://api.github.com"
 const GITHUB_HEADERS = {
-    Accept: "application/vnd.github.com", 
-    Authorization: `Bearer ${ process.env.GITHUB_TOKEN || ""}`
+    Accept: "application/vnd.github.com",
+    Authorization: `Bearer ${process.env.GITHUB_TOKEN || ""}`
 }
 /**
  * @param {string} Github Username 
@@ -13,16 +13,16 @@ const GITHUB_HEADERS = {
 
 async function getUserProfile(userName) {
     try {
-        const cacheKey = `user: ${ userName }`
+        const cacheKey = `user: ${userName}`
         const cached = cacheService.get(cacheKey)
 
         if (cached) {
-            console.log(`Cache Hit ${ cacheKey }`)
+            console.log(`Cache Hit ${cacheKey}`)
             return cached
         }
 
         console.log(`Cache Miss`)
-        const response = await axios.get(`${ GITHUB_API_BASE } / users / ${ userName }, { headers: GITHUB_HEADERS }`)
+        const response = await axios.get(`${GITHUB_API_BASE}/users/${userName}`, { headers: GITHUB_HEADERS })
 
         const profile = { // Ready response for user 
             login: response.data.login,
@@ -37,25 +37,72 @@ async function getUserProfile(userName) {
         }
 
         cacheService.set(cacheKey, profile) // Cache data for future return profile 
-        return cached
+        return profile
 
     } catch (error) {
-        console.log(`Some error occured while fetching ${ error?.message || "Github API Error" }`) 
-        return null 
+        console.log(`Some error occured while fetching ${error?.message || "Github API Error"}`)
+        return null
     }
 }
 
+/**
+ * 
+ * @param {string} userName - Github Username
+ * @param {number} pageNo Page number for pagination
+ * @returns Github repos + pagination info
+ */
 
-async function getUserRepo(userName, pageNo = 1) {
+async function getUserRepos(userName, pageNo = 1) {
     try {
-        const cacheKey = ` repos: ${ userName }: ${ pageNo }`
+        const cacheKey = ` repos: ${userName}: ${pageNo}`
         const cached = cacheService.get(cacheKey)
 
         if (cached) {
             console.log("Cache hit for github repo find " + cached)
             return cached
         }
+
+        const response = await axios.get(`${GITHUB_API_BASE}/users/${userName}/repos`, {
+            headers: GITHUB_HEADERS,
+            params: {
+                per_page: 30,
+                page:pageNo,
+                sort: "updated"
+            }
+        })
+
+        const repos = response.data.map((repo) => ({
+            id: repo.id,
+            name: repo.name,
+            description: repo.description,
+            language: repo.language,
+            stargazers_count: repo.stargazers_count,
+            updated_at: repo.updated_at,
+            html_url: repo.html_url,
+            open_issues_count: repo.open_issues_count,
+            default_branch: repo.default_branch,
+            forks_count: repo.forks_count
+        }))
+
+        const linkHeader = response.headers["link"]
+        const hasNextPage = linkHeader ? linkHeader.includes(`rel=next`) : false
+
+        const result = {
+            repos,
+            hasNextPage,
+            currentPage:pageNo
+        }
+
+        cacheService.set(cacheKey , result)
+        return result
+
     } catch (error) {
-        
+        console.log("Error while fetching repos from github " + error?.message)
+        return null
     }
 }
+
+module.exports = Object.freeze({
+    getUserProfile,
+    getUserRepos
+})
